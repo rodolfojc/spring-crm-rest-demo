c5f43a5e76a8bfc9378d09335753b2104128f528
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-05-31 21:50:34 +0100

    Getting a Sigle Customer
    Demo

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +25,12 @@ public class CustomerRestController {
 		return customerService.getCustomers();
 	}
 	
+	//ADD MAPPING FOR GET CUSTOMERS/{CUSTOMERSID - SINGLE CUSTOMER
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		
+		Customer theCustomer = customerService.getCustomer(customerId);
+				
+		return theCustomer;
+	}
 }
