c6e65e47735b6eb9d4fdb7f711b88ef96d77b641
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-06-01 21:11:09 +0100

    Update Customer
    Demo

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,15 @@ public class CustomerRestController {
 		theCustomer.setId(0);
 		customerService.saveCustomer(theCustomer);
 		
+		return theCustomer;
+	}
+	
+	//ADD MAPPING FOR PUT / CUSTOMERS - UPDATE AN EXISTING CUSTOMER
+	@PutMapping("/customers")
+	public Customer updateCustomer (@RequestBody Customer theCustomer) {
+			
+		customerService.saveCustomer(theCustomer);
+			
 		return theCustomer;
 	}
 	
