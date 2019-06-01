2ebf17ca4f5704b45b16a6739b598a4de79313fe
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-06-01 20:44:39 +0100

    Update CustomerRestExceptionHandler.java
    Demo

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
@@ -1,12 +1,28 @@
 package com.luv2code.springdemo.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class CustomerRestExceptionHandler {
 
 	//ADD AN EXCEPTION HANDLER OFR CUSTOMERNORFOUNDEXCEPTION
-	
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(CustomerNotFoundException exc) {
+		
+		//CREATE CUSTOMERERRORREPONSE
+		CustomerErrorResponse error = new CustomerErrorResponse(
+				HttpStatus.NOT_FOUND.value(),
+				exc.getMessage(),
+				System.currentTimeMillis()
+				);
+		
+		//RETURN RESPONSEENTITY
+		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
 	
 	//ADD EXCEPTION HANDLER FOR ANY EXCEPTION - GENERIC
 	
